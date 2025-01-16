0b91c4f7eeb734d809fc7a7ed41664a499cd7ea3
alaka004 <alaka9704@gmail.com>
2025-01-16 09:49:12 +0530

    Create add2num.c

---
+++ add2num.c
@@ -0,0 +1,14 @@
+# demo
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+printf("Enter 1st no :");
+scanf("%d",&a);
+printf("Enter 2nd no :");
+scanf("%d",&b);
+c=a+b;
+printf("sum of %d and %d is %d",a,b,c);
+getch();
+}
